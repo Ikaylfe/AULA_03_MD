9cbc207592634f9918aa9888b2317e8cd02f164d
Ikaylfe <kayane.brito.7@gmail.com>
2026-05-19 21:15:00 -0300

    corrigindo coluna da linha 40

--- aula03/atividade_aula03_01.py
+++ aula03/atividade_aula03_01.py
@@ -24,22 +24,22 @@ print(df_custos_moveis[['Produto','Total arrecadado']])
 array_custo_moveis = np.array(df_custos_moveis['Total arrecadado'])
 
 
-print('\nMédidas de tendência central')
-print(100*"_")
+# print('\nMédidas de tendência central')
+# print(100*"_")
 
-media = np.mean(array_custo_moveis)
-print(f'\nMédia dos custos: {media:.2f}')
+# media = np.mean(array_custo_moveis)
+# print(f'\nMédia dos custos: {media:.2f}')
 
-mediana = np.median(array_custo_moveis)
-print(f'\nMediana dos custos: {mediana:.2f}')
+# mediana = np.median(array_custo_moveis)
+# print(f'\nMediana dos custos: {mediana:.2f}')
 
 # q1 =np.quantile(df_custos_moveis, 0.25) # 25%
 # q2 =np.quantile(df_custos_moveis, 0.50) # 50%
 # q3 =np.quantile(df_custos_moveis, 0.75) # 75%
 
-q1 = np.quantile(df_custos_moveis['Preco'], 0.25)
-q2 = np.quantile(df_custos_moveis['Preco'], 0.50)
-q3 = np.quantile(df_custos_moveis['Preco'], 0.75)
+q1 = np.quantile(df_custos_moveis['Total arrecadado'], 0.25)
+q2 = np.quantile(df_custos_moveis['Total arrecadado'], 0.50)
+q3 = np.quantile(df_custos_moveis['Total arrecadado'], 0.75)
 
 
 print('\nMédidas de posição')
